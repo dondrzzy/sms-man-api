48195f989574a95a5dbbbe76438259dddee2113e
dondrzzy <sibo.donald16@gmail.com>
2019-06-26 20:31:37 +0300

    add files back

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ const conn = require('./config/database');
 conn.connect();
 
 // routes
-// const contactsRouter = require('./routes/contactRoutes');
-// const messageRouter = require('./routes/messageRoutes');
+const contactsRouter = require('./routes/contactRoutes');
+const messageRouter = require('./routes/messageRoutes');
 
 var app = express();
 
@@ -25,24 +25,24 @@ app.use(bodyParser.urlencoded({
 app.get('/', (req, res) => {
   res.send('Welcome to SMS-MAN-API');
 });
-// app.use('/api/v1/contacts', contactsRouter);
+app.use('/api/v1/contacts', contactsRouter);
 
-// app.use(function(req, res, next){
-//   const token = req.headers['token-x'];
-//   if(!token){
-//       res.status(400).json({error:'No token provided'});
-//   }else{
-//     jwt.verify(token, config.secret, function(err, decoded){
-//       if (err) {
-//         res.status(400).json({token: false, error:'token Invalid: '+err});}
-//       else{
-//         req.decoded = decoded;
-//         console.log('decoded: ', decoded);
-//         next(); 
-//       }
-//     });
-//   }
-// });
-// app.use('/api/v1/messages', messageRouter);
+app.use(function(req, res, next){
+  const token = req.headers['token-x'];
+  if(!token){
+      res.status(400).json({error:'No token provided'});
+  }else{
+    jwt.verify(token, config.secret, function(err, decoded){
+      if (err) {
+        res.status(400).json({token: false, error:'token Invalid: '+err});}
+      else{
+        req.decoded = decoded;
+        console.log('decoded: ', decoded);
+        next(); 
+      }
+    });
+  }
+});
+app.use('/api/v1/messages', messageRouter);
 
 module.exports = app;
