6619bd48f088e059e409f14ee3174a2df8e67d15
dondrzzy <sibo.donald16@gmail.com>
2019-06-26 20:20:04 +0300

    add production settings

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,15 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
-if (process.env.NODE_ENV === 'development') {
-	module.exports = {
-    uri: process.env.DB_URI,
-    secret: process.env.SECRET_KEY,
-    db: process.env.DB_NAME
-  }
-}
-
 if (process.env.NODE_ENV === 'testing') {
 	module.exports = {
     uri: process.env.TEST_DB_URI,
     secret: process.env.TEST_SECRET_KEY,
     db: process.env.TEST_DB_NAME
+  }
+} else {
+  module.exports = {
+    uri: process.env.DB_URI,
+    secret: process.env.SECRET_KEY,
+    db: process.env.DB_NAME
   }
 }
