715cbb0a40c09a1e22bf15804784a0bbe56b08c2
dondrzzy <sibo.donald16@gmail.com>
2019-06-26 20:10:38 +0300

    test db failure

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ var cors = require('cors');
 var path = require('path');
 const jwt = require('jsonwebtoken');
 
-// config
-const config = require('./config');
+// // config
+// const config = require('./config');
 
-const conn = require('./config/database');
-conn.connect();
+// const conn = require('./config/database');
+// conn.connect();
 
-// routes
-const contactsRouter = require('./routes/contactRoutes');
-const messageRouter = require('./routes/messageRoutes');
+// // routes
+// const contactsRouter = require('./routes/contactRoutes');
+// const messageRouter = require('./routes/messageRoutes');
 
 var app = express();
 
@@ -25,24 +25,24 @@ app.use(bodyParser.urlencoded({
 app.get('/', (req, res) => {
   res.send('Welcome to SMS-MAN-API');
 });
-app.use('/api/v1/contacts', contactsRouter);
+// app.use('/api/v1/contacts', contactsRouter);
 
-app.use(function(req, res, next){
-  const token = req.headers['token-x'];
-  if(!token){
-      res.status(400).json({error:'No token provided'});
-  }else{
-    jwt.verify(token, config.secret, function(err, decoded){
-      if (err) {
-        res.status(400).json({token: false, error:'token Invalid: '+err});}
-      else{
-        req.decoded = decoded;
-        console.log('decoded: ', decoded);
-        next(); 
-      }
-    });
-  }
-});
-app.use('/api/v1/messages', messageRouter);
+// app.use(function(req, res, next){
+//   const token = req.headers['token-x'];
+//   if(!token){
+//       res.status(400).json({error:'No token provided'});
+//   }else{
+//     jwt.verify(token, config.secret, function(err, decoded){
+//       if (err) {
+//         res.status(400).json({token: false, error:'token Invalid: '+err});}
+//       else{
+//         req.decoded = decoded;
+//         console.log('decoded: ', decoded);
+//         next(); 
+//       }
+//     });
+//   }
+// });
+// app.use('/api/v1/messages', messageRouter);
 
 module.exports = app;
